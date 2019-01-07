16a21569c48696aaebbd60866bbf0ee389136d1e
Lasse Larsen <>
2019-01-07 18:32:45 +0100

    Updates

---
+++ precache-manifest.8181cada9ff44d3576c94a30e1f02a88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e0bb0d1e79ec9a38585",
+    "url": "/sorting-filtering-list-app/static/js/runtime~main.0e0bb0d1.js"
+  },
+  {
+    "revision": "3e5a55f60c89bd3f65b6",
+    "url": "/sorting-filtering-list-app/static/js/main.3e5a55f6.chunk.js"
+  },
+  {
+    "revision": "10cc0dbf27ca5107c547",
+    "url": "/sorting-filtering-list-app/static/js/3.10cc0dbf.chunk.js"
+  },
+  {
+    "revision": "69366d21876242993da5",
+    "url": "/sorting-filtering-list-app/static/js/2.69366d21.chunk.js"
+  },
+  {
+    "revision": "a021e3a0eaac22b8de9e",
+    "url": "/sorting-filtering-list-app/static/js/1.a021e3a0.chunk.js"
+  },
+  {
+    "revision": "a021e3a0eaac22b8de9e",
+    "url": "/sorting-filtering-list-app/static/css/1.a18eff08.chunk.css"
+  },
+  {
+    "revision": "d8eca207105f59a436817b8a6ffbc6c1",
+    "url": "/sorting-filtering-list-app/index.html"
+  }
+];
