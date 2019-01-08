8a3a72475900928d273cc7f3743fe115e5de140b
Lasse Larsen <>
2019-01-08 15:35:08 +0100

    Updates

---
+++ precache-manifest.fc712d1a69e8459e83e9ef9bd99bab6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c25fbba564bcc9a2847",
+    "url": "/sorting-filtering-list-app/static/js/runtime~main.8c25fbba.js"
+  },
+  {
+    "revision": "3e5a55f60c89bd3f65b6",
+    "url": "/sorting-filtering-list-app/static/js/main.3e5a55f6.chunk.js"
+  },
+  {
+    "revision": "10cc0dbf27ca5107c547",
+    "url": "/sorting-filtering-list-app/static/js/3.10cc0dbf.chunk.js"
+  },
+  {
+    "revision": "69366d21876242993da5",
+    "url": "/sorting-filtering-list-app/static/js/2.69366d21.chunk.js"
+  },
+  {
+    "revision": "a9e502508ded3db6f343",
+    "url": "/sorting-filtering-list-app/static/js/1.a9e50250.chunk.js"
+  },
+  {
+    "revision": "a9e502508ded3db6f343",
+    "url": "/sorting-filtering-list-app/static/css/1.a18eff08.chunk.css"
+  },
+  {
+    "revision": "c4056c75273030e87bc4956533fad80e",
+    "url": "/sorting-filtering-list-app/index.html"
+  }
+];
--- src/components/products/ProductCategoryRow.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export const ProductCategoryRow = ({ category }) => (<strong style={{ padding: 10 }}>
-  {category}
-</strong>);
